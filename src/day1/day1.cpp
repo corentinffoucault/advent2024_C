5f7c6770a712292fed80a8fe9c1eaa266adbdacc
corentin <corentinf@free.fr>
2025-03-11 18:22:52 +0100

    first day-bis

--- src/day1/day1.cpp
+++ src/day1/day1.cpp
@@ -12,15 +12,13 @@ namespace fs = std::filesystem;
 using Container = std::vector<std::string>;
 
 int day1::run(fs::path currentPath) {
-    const fs::path path2 = currentPath.append("resources").append("day1.txt");
-    cout << path2 << "\n";
     string my_text;
-    ifstream file_content(path2);
     std::vector<int> part1;
     std::vector<int> part2;
     std::regex delimiter{ "\\s+" };
-    char* output;
     
+    const fs::path path2 = currentPath.append("resources").append("day1.txt");
+    ifstream file_content(path2);
     while (getline(file_content, my_text)) {
         Container line(std::sregex_token_iterator(my_text.begin(), my_text.end(), delimiter, -1), {});
         part1.push_back(std::stol(line[0]));
@@ -32,7 +30,6 @@ int day1::run(fs::path currentPath) {
     sort(part2.begin(), part2.end());
 
     long result = 0;
-
     for each(long item in part1) {
         long counter = std::count_if(part2.begin(), part2.end(), [&](int x) { return x == item; });
         result += item * counter;
